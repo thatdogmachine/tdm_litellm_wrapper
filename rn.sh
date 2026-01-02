647a941f1b5cd5222cc36bc7d80f2229a5925de0
Ewan Nisbet <ewan-git@thatdogmachine.com>
2026-01-02 20:48:47 +0000

    chore: add nix experimental flags

--- rn.sh
+++ rn.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
-
-nix develop --fallback # -c zsh
+# UNFREE / impure originally but unneeded for redis but left in place regardless
+export NIXPKGS_ALLOW_UNFREE=1
+CURRENT_DIR="$(pwd)"
+nix develop --extra-experimental-features ca-derivations --extra-experimental-features impure-derivations --fallback --impure
